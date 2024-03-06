441d11e18ed716b1390f764a7b98aacfa48075df
tigerkaplane <husniyeerparun@gmail.com>
2024-03-06 18:37:20 +0000

    variations added

---
+++ app.js
@@ -0,0 +1,13 @@
+// petrol station 
+
+/*
+Diesel : 154.4
+Petrol : 144.6
+Lpg    : 0.50
+
+1. fuel type
+2. how many litre */
+
+// variations for fuel type
+
+let diesel = 154.4, petrol = 144.6, lpg = 0.50;
