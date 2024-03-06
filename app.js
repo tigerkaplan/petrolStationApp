d21c58444439d2be5489591feecc16ec5098dc57
tigerkaplane <husniyeerparun@gmail.com>
2024-03-06 21:31:46 +0000

    diesel cost added

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// petrol station 
+// petrol station
 
 /*
 Diesel : 154.4
@@ -10,20 +10,48 @@ Lpg    : 0.50
 
 // variations for fuel type
 
-let diesel = 154.4, petrol = 144.6, lpg = 0.50;
+let diesel = 154.4,
+  petrol = 144.6,
+  lpg = 0.5;
 const newLine = "\r\n";
 
-const chosenFuel = "1- Diesel" + newLine + "2- Petrol" + newLine + "3- Lpg" + newLine + "Please, choose your fuel type";
+const chosenFuel =
+  "1- Diesel" +
+  newLine +
+  "2- Petrol" +
+  newLine +
+  "3- Lpg" +
+  newLine +
+  "Please, choose your fuel type";
 
 // Prompt for choose fuel type
 
 let fuelType = prompt(chosenFuel);
+let fuelCostLiter = Number(prompt("Please, enter liter"));
+let balance = Number(prompt("Please, enter your balance"));
+//cost
 
 if (fuelType == "1") {
-    // Diesel
-} else if (fuelType == "2") {
-    // Petrol
-} else if (fuelType == "3") {
-    // Lpg
-} else ( "Please, enter your fuel type");
+  // Diesel
+  let fuelCost = diesel * fuelCostLiter;
+  if (fuelCost < balance) {
+    //sufficient amount
+    change = balance - fuelCost
+    alert ("Successful transition"+newLine
+    + "Fuel Cost: " + fuelCost + newLine
+    + "Change : " + change + newLine
+    + "Thank you for payment");
 
+  } if (fuelCost > balance) {
+    // insufficient amount
+    alert ("Insufficient amount." + newLine
+    + "Fuel Cost: " + fuelCost +newLine
+    + "Balance : " + balance+newLine
+    + "Missing Balance : " + (fuelCost - balance) + newLine);
+  } 
+} else if (fuelType == "2") {
+  // Petrol
+  
+} else if (fuelType == "3") {
+  // Lpg
+} else "Please, enter your fuel type";
