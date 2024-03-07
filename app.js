eff74a88e10a6f179a0c920592b7e2df257c2504
tigerkaplane <husniyeerparun@gmail.com>
2024-03-07 00:21:50 +0000

    adding not correct fuel type

--- app.js
+++ app.js
@@ -27,129 +27,138 @@ const chosenFuel =
 // Prompt for choose fuel type
 
 let fuelType = prompt(chosenFuel);
-let fuelCostLiter = Number(prompt("Please, enter liter"));
-let balance = Number(prompt("Please, enter your balance"));
-//cost
 
-// ********* Diesel *********
-if (fuelType == "1") {
-  // Diesel
+// ! if chosen one of the correct fuel types =====>
 
-  let fuelCostDiesel = diesel * fuelCostLiter;
+if (fuelType == "1" || fuelType == "2" || fuelType == "3") {
+  let fuelCostLiter = Number(prompt("Please, enter liter"));
+  let balance = Number(prompt("Please, enter your balance"));
+  //cost
 
-  // Sufficient Amount
+  // ********* Diesel *********
+  if (fuelType == "1") {
+    // Diesel
 
-  if (fuelCostDiesel < balance) {
-    change = balance - fuelCostDiesel;
-    alert(
-      "Successful transition" +
-        newLine +
-        "Fuel Cost: " +
-        fuelCostDiesel +
-        newLine +
-        "Change : " +
-        change +
-        newLine +
-        "Thank you for payment"
-    );
+    let fuelCostDiesel = diesel * fuelCostLiter;
+
+    // Sufficient Amount
+
+    if (fuelCostDiesel < balance) {
+      change = balance - fuelCostDiesel;
+      alert(
+        "Successful transition" +
+          newLine +
+          "Fuel Cost: " +
+          fuelCostDiesel +
+          newLine +
+          "Change : " +
+          change +
+          newLine +
+          "Thank you for payment"
+      );
+    }
+
+    // Insufficient Amount
+
+    if (fuelCostDiesel > balance) {
+      alert(
+        "Insufficient amount" +
+          newLine +
+          "Fuel Cost: " +
+          fuelCostDiesel +
+          newLine +
+          "Balance : " +
+          balance +
+          newLine +
+          "Missing Amount : " +
+          (fuelCostDiesel - balance) +
+          newLine
+      );
+    }
+
+    // ********* Petrol *********
+  } else if (fuelType == "2") {
+    // Petrol
+
+    let fuelCostPetrol = petrol * fuelCostLiter;
+
+    // Sufficient Amount
+
+    if (fuelCostPetrol < balance) {
+      change = balance - fuelCostPetrol;
+      alert(
+        "Successful transition" +
+          newLine +
+          "Fuel Cost Petrol:" +
+          fuelCostPetrol +
+          newLine +
+          "Change:" +
+          change +
+          newLine +
+          "Thank you for payment"
+      );
+    }
+    // Insufficient Amount
+
+    if (fuelCostPetrol > balance) {
+      alert(
+        "Insufficient Amount" +
+          newLine +
+          "Fuel Cost Petrol:" +
+          fuelCostPetrol +
+          newLine +
+          "Balance:" +
+          balance +
+          newLine +
+          "Missing Amount:" +
+          (fuelCostPetrol - balance) +
+          newLine
+      );
+    }
+
+    // ********* LPG *********
+  } else if (fuelType == "3") {
+    // Lpg
+
+    let fuelCostLpg = lpg * fuelCostLiter;
+
+    // Sufficient Amount
+
+    if (fuelCostLpg < balance) {
+      change = balance - fuelCostLpg;
+      alert(
+        "Successful transition" +
+          newLine +
+          "Fuel Cost Lpg:" +
+          fuelCostLpg +
+          newLine +
+          "Change:" +
+          change +
+          newLine +
+          "Thank you for payment"
+      );
+    }
+
+    // Insufficient Amount
+
+    if (fuelCostLpg > balance) {
+      alert(
+        "Insufficient Amount" +
+          newLine +
+          "Fuel Cost Lpg:" +
+          fuelCostLpg +
+          newLine +
+          "Balance:" +
+          balance +
+          newLine +
+          "Missing Amount:" +
+          (fuelCostLpg - balance) +
+          newLine
+      );
+    }
   }
 
-  // Insufficient Amount
-
-  if (fuelCostDiesel > balance) {
-    alert(
-      "Insufficient amount" +
-        newLine +
-        "Fuel Cost: " +
-        fuelCostDiesel +
-        newLine +
-        "Balance : " +
-        balance +
-        newLine +
-        "Missing Amount : " +
-        (fuelCostDiesel - balance) +
-        newLine
-    );
-  }
-
-  // ********* Petrol *********
-} else if (fuelType == "2") {
-  // Petrol
-
-  let fuelCostPetrol = petrol * fuelCostLiter;
-
-  // Sufficient Amount
-
-  if (fuelCostPetrol < balance) {
-    change = balance - fuelCostPetrol;
-    alert(
-      "Successful transition" +
-        newLine +
-        "Fuel Cost Petrol:" +
-        fuelCostPetrol +
-        newLine +
-        "Change:" +
-        change +
-        newLine +
-        "Thank you for payment"
-    );
-  }
-  // Insufficient Amount
-
-  if (fuelCostPetrol > balance) {
-    alert(
-      "Insufficient Amount" +
-        newLine +
-        "Fuel Cost Petrol:" +
-        fuelCostPetrol +
-        newLine +
-        "Balance:" +
-        balance +
-        newLine +
-        "Missing Amount:" +
-        (fuelCostPetrol - balance) +
-        newLine
-    );
-  }
-
-  // ********* LPG *********
-} else if (fuelType == "3") {
-  // Lpg
-
-  let fuelCostLpg = lpg * fuelCostLiter;
-
-  // Sufficient Amount
-
-  if (fuelCostLpg < balance) {
-    change = balance - fuelCostLpg;
-    alert(
-      "Successful transition" +
-        newLine +
-        "Fuel Cost Lpg:" +
-        fuelCostLpg +
-        newLine +
-        "Change:" +
-        change +
-        newLine +
-        "Thank you for payment"
-    );
-  }
-
-  // Insufficient Amount
-
-  if (fuelCostLpg > balance) {
-    alert(
-      "Insufficient Amount" +
-        newLine +
-        "Fuel Cost Lpg:" +
-        fuelCostLpg +
-        newLine +
-        "Balance:" +
-        balance +
-        newLine +
-        "Missing Amount:" +
-        (fuelCostLpg - balance) +
-        newLine
-    );
-  }
-} else "Please, enter your fuel type";
+  // ! if not chosen one of the correct fuel types =====>
+} else {
+  alert("Please, enter correct fuel type");
+}
